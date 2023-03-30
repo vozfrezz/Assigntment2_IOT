62112e49f1a798398df48b09540f0d26d14838c9
vozfrezz <tuekhac@gmail.com>
2023-03-30 21:48:31 +0700

    Fix error in assignment 2

--- src/analysis.c
+++ src/analysis.c
@@ -16,42 +16,58 @@ int main() {
   filetoStr(fileStr);
   char filecpy[MAXLENGTHFILE]; // declare an array to store the modified string
   char keyword[] = "\"cmd\":\"set\"";
-  char type_id[] = "\"type\":\"switch\"";
+  char type_id[] =
+      "\"type\":\"switch\",\"data\":[\"zwave-"; //"type":"switch","data":["zwave-
+
   char *token;
   char *token_1;
+  char *token_2;
+  char *token_3;
   const char *delimiters = "\'\n\'";
 
-  char network_id[100];
-  char device_id[100];
+  char network_id[10][10];
+  char device_id[10][10];
+  int network_element = 0;
+  int count = 0;
+  char network_code[100];
+  char *network_id_name;
 
   // printf("\n delemiters : %s",delimiters);
 
   char network_token[MAXLENGTHFILE] = "";
   token_1 = strtok(fileStr, delimiters);
   printf("Token: %s\n", token_1);
-  token_1 = strstr(token_1, type_id);
-  // token_1 = strtok(NULL, delimiters);
-  // if (token_1 && strstr(token_1, type_id)) {
-  printf("Token: %s\n", token_1);
-  char *network_id_name = strstr(token_1, "zwave-");
+  while (token_1 != NULL) {
+    token_1 = strtok(NULL, delimiters);
+    if (token_1 != NULL) {
+      token_3 = strstr(token_1, keyword);
+      if (token_3 != NULL) {
+        printf("\ncmd_Set: %s\n", token_3);
+        token_2 = strstr(token_3, type_id);
+        if (token_2 != NULL) {
+          printf("type_Swich: %s\n", token_2);
+          strncpy(network_code, token_2 + 31, 4);
+          network_code[4] = '\0';
+          printf("\n string: %s", network_code);
+      /*  for (int i = 0; i < network_element; i++) {
+          if (strcmp(network_code, network_id[i]) == 0) {
+            count = 1;
+            break;
+          };
+        };
+        if (!count) {
+          strcpy(network_id[network_element], network_code);
+          strncpy(device_id[network_element], token_2 + strlen(token_2) - 1, 1);
+          network_element++;
+        };
+      */}
+      }
+    };
+  };
 
-  printf("Token: %s\n", network_id_name);
-
-  char *token_2 = strtok(network_id_name, "\"]");
-
-  printf("Token: %s\n", token_2);
-
-  strncpy(network_id, token_2 + 6, 4);
-  strncpy(device_id, token_2 + strlen(token_2) - 1, 1);
-  // if (token_1 != NULL) {
-  //};
-  // };
-  printf("\nnetwork: %s", network_id);
-  printf("\ndevice: %s", device_id);
-  /* while (token_1 != NULL) {
-   };
-   */
-
+  for (int i = 0; i < network_element; i++) {
+    printf("network_id[%d]: %s\n", i, network_id[i]);
+  };
   return 0;
 };
 
@@ -59,16 +75,16 @@ int filetoStr(char *str) {
 
   int status; // khoi tao 1 bien status co dang so nguyen
 
-  FILE *fp = NULL; // khoi tao 1 bien con tro cau truc fp va gan gia tri ban dau
-                   // la NULL
+  FILE *fp = NULL; // khoi tao 1 bien con tro cau truc fp va gan gia tri ban
+                   // dau la NULL
 
   fp = fopen(FILENAME,
-             "r"); // mo file co ten la FILENAME hay 'log.txt' va mo o che do r
-                   // "read only" va con tro fp se tro toi day.
+             "r"); // mo file co ten la FILENAME hay 'log.txt' va mo o che do
+                   // r "read only" va con tro fp se tro toi day.
                    //  dung fp de thuc hien doc va ghi du lieu
   if (fp == NULL) {
-    //   printf("File does not exist\n"); // kiem tra neu fp == NULL thi tra ve
-    //   ket
+    //   printf("File does not exist\n"); // kiem tra neu fp == NULL thi tra
+    //   ve ket
     // qua la file rong
     return -1; // tra ve gia tri la -1
   }
